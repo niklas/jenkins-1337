6b7f194ba8d0af7d7ab437640ae5f1dc15e5d424
Niklas Hofer <niklas+dev@lanpartei.de>
2012-10-10 20:55:31 +0200

    detect console by looking at URL

--- jenkins.js
+++ jenkins.js
@@ -1,6 +1,6 @@
 jQuery(document).ready(function() {
-        // If the link in menu to "Console Output" is "active" (bold)
-        if (jQuery('a[href$="/console"] > b').length > 0) {
+        // If the URL path contains 'console'
+        if (window.location.pathname.indexOf('console') > 1) {
                 jQuery('#main-table').addClass('showing-console');
         }
 
